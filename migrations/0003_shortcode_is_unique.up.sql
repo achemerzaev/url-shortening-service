6de1353284e9fcb0d5d39132c845dea37884d281
boretsotets <cachow@bk.ru>
2025-10-04 00:42:30 +0300

    проверка ci

--- migrations/0003_shortcode_is_unique.up.sql
+++ migrations/0003_shortcode_is_unique.up.sql
@@ -1,3 +1,3 @@
 ALTER TABLE urls
-ADD CONSTRAINT unique_url UNIQUE (Url);
-ADD CONSTRAINT unique_shortcode UNIQUE (ShortCode);
+ADD CONSTRAINT unique_url UNIQUE (Url),
+    CONSTRAINT unique_shortcode UNIQUE (ShortCode);
